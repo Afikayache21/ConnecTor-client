ef65a6fcbcda0b79398a188348a7ed86e155caae
Afikayache21 <122890984+Afikayache21@users.noreply.github.com>
2024-08-04 17:14:23 +0300

    Login update

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './mobileLogin.scss'
 import './desktopLogin.scss'
 import { Link } from 'react-router-dom'
@@ -8,21 +8,23 @@ import { useNavigate } from 'react-router-dom'
 
 const url = 'http://localhost:5000/api/auth/login'
 
-const navigate = useNavigate();
 
 export default function login() {
-
+    const navigate = useNavigate();
+    const [user, setUser] = useState({ email: '', password: '' });
 
 
     const handleLogIn = async (e) => {
         e.preventDefault();
         try {
+            console.log(user);
+
             const response = await axios.post(url, {
-                email: email,
-                userPassword: password
+                Mail: user.email,
+                Password: user.password
             });
             localStorage.setItem('token', response.data.token);
-            console.log('Login Successful:', response.data,'token', response.data.token);
+            console.log('Login Successful:', response.data, 'token', response.data.token);
             navigate('/') // Redirect to home page after successful login
         } catch (error) {
             alert('Invalid credentials. Please try again.')
@@ -34,14 +36,18 @@ export default function login() {
         console.log('Logging in...');
         // Redirect to home page after successful login
     }
+
+    const handleChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
     return (
         <div className='login'>
             <h1>Login</h1>
             <form onSubmit={handleLogIn} className='login-card'>
                 <div className='right-side'>
                     <div className='inputs-form'>
-                        <input type="text" placeholder='Email' />
-                        <input type="password" placeholder='Password' />
+                        <input onChange={handleChange} name='email' type="text" placeholder='Email' />
+                        <input onChange={handleChange} name='password' type="password" placeholder='Password' />
                         <button type='submit'>Login</button>
                         <Link to='/register'>
                             <span>Don't have an account?</span>
